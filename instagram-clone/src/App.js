2e2338c50751fb80aaee76fe18087f2d1ea1fabe
An Doan <dtan42@gmail.com>
2020-09-15 10:17:46 +1000

    asd

---
+++ instagram-clone/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello An Doan</h1>
+      {/* Header*/}
+      <div className="app_header">
+        
+      </div>
+      {/* Posts*/}
+      {/* Header*/}
+      {/* Header*/}
+      {/* Header*/}
+
+    </div>
+  );
+}
+
+export default App;
